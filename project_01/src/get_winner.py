7defe340ea04c3da7812878eaf4883428e2cf2a3
Omnei <sethm1093@gmail.com>
2015-02-09 20:17:39 -0600

    Changed get_winner to branching if statement
    performance increase

--- project_01/src/get_winner.py
+++ project_01/src/get_winner.py
@@ -7,108 +7,116 @@ from match_options import *#get_best_match, get_bigram_list_match_tweets, get_bi
 def get_winner(category, tweets, parsed_list):
             # HOSTS
     # BIGRAMS
-    lot_hosts = ['host']
-
-    ''' MOVIE SECTION '''
-    # BEST MOVIE, DRAMA
-    # UNIGRAM
-    lot_best_drama_movie = ['best', 'picture'] # Number 3 response with this
-    #lot_best_drama_movie_opt = ['film', 'movie']
-    # BEST MOVIE, MUS OR COM
-    # BIGRAM
-    lot_best_mus_com_movie = ['best', 'picture'] # Number 3 response with this
-    lot_best_mus_com_movie_opt = ['com', 'mus']
-    # BEST ACTOR/ACTRESS, DRAMA
-    # BIGRAMS
-    lot_best_actor_drama_movie = ['best', 'drama', 'actor'] # Number 1 response with this
-    lot_best_actress_drama_movie = ['best', 'drama', 'actress'] # Number 1 response with this
-    lot_best_actress_drama_movie_opt = ['movie', 'film']
-    # BEST ACTOR/ACTRESS, COMEDY OR MUSICAL
-    # BIGRAMS
-    lot_best_actor_mus_com = ['best', 'actor'] # Number 1 response with this
-    lot_best_actor_mus_com_opt = ['com', 'mus']
-    lot_best_actress_mus_com = ['best', 'actress'] # Number 1 response with this
-    lot_best_actress_mus_com_opt = ['com', 'mus']
-    # BEST SUPPORTING ACTOR/ACTRESS, ALL MOVIES
-    # BIGRAMS
-    lot_best_supporting_actor = ['best', 'support', 'actor'] # Number 1 response with this
-    lot_best_supporting_actress = ['best', 'support', 'actress'] # Number 1 response with this
-    # BEST DIRECTOR
-    #BIGRAM
-    lot_best_director = ['best', 'director'] # Number 1 response with this
-    # BEST SCREENPLAY
-    # BIGRAM
-    lot_best_screenplay = ['best', 'screenplay'] # Number 1 response with this
-    # BEST ORIGINAL SCORE
-    # BIGRAM
-    lot_best_original_score = ['best', 'original', 'score'] # Number 1 response with this
-    # BEST ORIGINAL SONG
-    # UNIGRAM
-    lot_best_original_song = ['best', 'original', 'song'] # Number 1 response with this
-    # BEST ANIMATED FEATURE FILM
-    # UNIGRAM
-    lot_best_animated_film = ['best', 'animated'] # Number 1 response with this
-    # BEST FOREIGN LANGUAGE FILM
-    # UNIGRAM
-    lot_best_foreign_film = ['best', 'foreign', 'film', 'language'] # Number 1 response with this
-
-    ''' TV SECTION '''
-    # BEST TV, DRAMA
-    # UNIGRAM
-    lot_best_drama_tv = ['best', 'drama', 'tv'] # Number 1 response with this
-    # BEST TV, MUS OR COM
-    # UNIGRAM
-    lot_best_mus_com_tv = ['best', 'series', 'com'] # Number 8 response with this
-    # BEST ACTOR/ACTRESS, DRAMA
-    # BIGRAMS - this one we may need to match up with the nominations
-    lot_best_actor_drama_tv = ['best', 'drama', 'actor', 'show'] # Number 2 response with this
-    # lot_best_actor_drama_tv = ['best', 'drama', 'actor'] # Number 3 response with this
-    lot_best_actress_drama_tv = ['best', 'drama', 'actress'] # Number 1 response with this
-    # BEST ACTOR/ACTRESS, COMEDY OR MUSICAL
-    # BIGRAMS
-    lot_best_actor_tv_mus_com = ['best', 'actor', 'tv'] # Number 1 response with this
-    lot_best_actor_tv_mus_com_opt = ['com', 'mus']
-    lot_best_actress_tv_mus_com = ['best', 'actress', 'tv'] # Number 1 response with this, but it's weak
-    lot_best_actress_tv_mus_com_opt = ['com', 'mus']
-    # BEST ACTOR/ACTRESS, MINISERIES/TV FILM
-    lot_best_actor_miniseries = ['best', 'actor', 'miniseries'] # Number 1 response with this
-    lot_best_actress_miniseries = ['best', 'actress', 'miniseries'] # Number 1 response with this
-    # BEST SUPPORTING ACTOR/ACTRESS, ALL TV
-    # BIGRAMS
-    lot_best_supporting_actor_tv = ['best', 'support', 'actor', 'series'] # Number 1 response with this
-    lot_best_supporting_actress_tv = ['best', 'support', 'actress', 'series'] # Tied for first like this
-    # BEST MINISERIES or TV FILM
-    # BIGRAMS
-    lot_best_miniseries = ['best', 'miniseries'] # Number 3 response with this
-
-    query_options = {}
-    #Movie
-    query_options['best movie drama'] = get_unigram_list_match_tweets_lax(tweets, lot_best_drama_movie)#, lot_best_drama_movie_opt)
-    query_options['best actress drama'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_drama_movie, lot_best_actress_drama_movie_opt)
-    query_options['best actor drama'] = get_bigram_list_match_tweets_lax(tweets, lot_best_actor_drama_movie)
-    query_options['best movie musical or comedy'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_mus_com_movie, lot_best_mus_com_movie_opt)
-    query_options['best actress musical or comedyres_best_actress'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_mus_com, lot_best_actress_mus_com_opt)
-    query_options['best actor musical or comedyres_best_actor'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actor_mus_com, lot_best_actor_mus_com_opt)
-    query_options['best animated movie'] = get_unigram_list_match_tweets_lax(tweets, lot_best_animated_film)
-    query_options['best foreign movie'] = get_unigram_list_match_tweets_lax(tweets, lot_best_foreign_film)
-    query_options['best supporting actress'] = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actress)
-    query_options['best supporting actor'] = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actor)
-    query_options['best director'] = get_bigram_list_match_tweets_lax(tweets, lot_best_director)
-    query_options['best screenplay'] = get_bigram_list_match_tweets_lax(tweets, lot_best_screenplay)
-    query_options['best original score'] = get_bigram_list_match_tweets_lax(tweets, lot_best_original_score)
-    query_options['best original song'] = get_unigram_list_match_tweets_lax(tweets, lot_best_original_song)
-    #TV
-    query_options['best tv series drama'] = get_unigram_list_match_tweets_lax(tweets, lot_best_drama_tv)
-    query_options['best actress tv drama'] = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_drama_tv)
-    query_options['best actor tv drama'] = get_bigram_list_match_tweets_lax(tweets, lot_best_actor_drama_tv)
-    query_options['best tv musical or comedy'] = get_unigram_list_match_tweets_lax(tweets, lot_best_mus_com_tv)#, lot_best_mus_com_tv_opt)
-    query_options['best actress tv musical or comedy'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_tv_mus_com, lot_best_actress_tv_mus_com_opt)
-    query_options['best actor tv musical or comedy'] = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actor_tv_mus_com, lot_best_actor_tv_mus_com_opt)
-    query_options['best tv movie'] = get_bigram_list_match_tweets_lax(tweets, lot_best_miniseries)
-    query_options['best actress tv movie'] = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_miniseries)
-    query_options['best actor tv movie'] = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_miniseries)
-    query_options['best supporting actress tv movie'] = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actress_tv)
-    query_options['best supporting actor tv movie'] = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actor_tv)
     
-    return get_best_match(query_options[category][0], parsed_list[category][:])
+
+    if category == 'host':
+        lot_hosts = ['host']
+        res = get_bigram_list_match_tweets(tweets, lot_hosts)
+
+    if category == 'best movie drama':
+        lot_best_drama_movie = ['best', 'picture'] # Number 3 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_drama_movie)#, lot_best_drama_movie_opt)
+
+    if category == 'best actress drama':
+        lot_best_actress_drama_movie = ['best', 'drama', 'actress'] # Number 1 response with this
+        lot_best_actress_drama_movie_opt = ['movie', 'film']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_drama_movie, lot_best_actress_drama_movie_opt)
+
+    if category == 'best actor drama':
+        lot_best_actor_drama_movie = ['best', 'drama', 'actor'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_actor_drama_movie)
+
+    if category == 'best movie musical or comedy':
+        lot_best_mus_com_movie = ['best', 'picture'] # Number 3 response with this
+        lot_best_mus_com_movie_opt = ['com', 'mus']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_mus_com_movie, lot_best_mus_com_movie_opt)
+
+    if category == 'best actress musical or comedy_best_actress':
+        lot_best_actress_mus_com = ['best', 'actress'] # Number 1 response with this
+        lot_best_actress_mus_com_opt = ['com', 'mus']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_mus_com, lot_best_actress_mus_com_opt)
+
+    if category == 'best actor musical or comedy_best_actor':
+        lot_best_actor_mus_com = ['best', 'actor'] # Number 1 response with this
+        lot_best_actor_mus_com_opt = ['com', 'mus']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actor_mus_com, lot_best_actor_mus_com_opt)
+
+    if category == 'best animated movie':
+        lot_best_animated_film = ['best', 'animated'] # Number 1 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_animated_film)
+
+    if category == 'best foreign movie':
+        lot_best_foreign_film = ['best', 'foreign', 'film', 'language'] # Number 1 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_foreign_film)
+
+    if category == 'best supporting actress':
+        lot_best_supporting_actress = ['best', 'support', 'actress'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actress)
+
+    if category == 'best supporting actor':
+        lot_best_supporting_actor = ['best', 'support', 'actor'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actor)
+
+    if category == 'best director':
+        lot_best_director = ['best', 'director'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_director)
+
+    if category == 'best screenplay':
+        lot_best_screenplay = ['best', 'screenplay'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_screenplay)
+
+    if category == 'best original score':
+        lot_best_original_score = ['best', 'original', 'score'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_original_score)
+
+    if category == 'best original song':
+        lot_best_original_song = ['best', 'original', 'song'] # Number 1 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_original_song)
+
+    if category == 'best tv series drama':
+        lot_best_drama_tv = ['best', 'drama', 'tv'] # Number 1 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_drama_tv)
+
+    if category == 'best actress tv drama':
+        lot_best_actress_drama_tv = ['best', 'drama', 'actress'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_drama_tv)
+
+    if category == 'best actor tv drama':
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_actor_drama_tv)
+
+    if category == 'best tv musical or comedy':
+        lot_best_mus_com_tv = ['best', 'series', 'com'] # Number 8 response with this
+        res = get_unigram_list_match_tweets_lax(tweets, lot_best_mus_com_tv)#, lot_best_mus_com_tv_opt)
+
+    if category == 'best actress tv musical or comedy':
+        lot_best_actress_tv_mus_com = ['best', 'actress', 'tv'] # Number 1 response with this, but it's weak
+        lot_best_actress_tv_mus_com_opt = ['com', 'mus']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actress_tv_mus_com, lot_best_actress_tv_mus_com_opt)
+
+    if category == 'best actor tv musical or comedy':
+        lot_best_actor_tv_mus_com = ['best', 'actor', 'tv'] # Number 1 response with this
+        lot_best_actor_tv_mus_com_opt = ['com', 'mus']
+        res = get_bigram_list_match_tweets_either_or_lax(tweets, lot_best_actor_tv_mus_com, lot_best_actor_tv_mus_com_opt)
+
+    if category == 'best tv movie':
+        lot_best_miniseries = ['best', 'miniseries'] # Number 3 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_miniseries)
+
+    if category == 'best actress tv movie':
+        lot_best_actress_miniseries = ['best', 'actress', 'miniseries'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_miniseries)
+
+    if category == 'best actor tv movie':
+        lot_best_actor_miniseries = ['best', 'actor', 'miniseries'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_actress_miniseries)
+
+    if category == 'best supporting actress tv movie':
+        lot_best_supporting_actress_tv = ['best', 'support', 'actress', 'series'] # Tied for first like this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actress_tv)
+
+    if category == 'best supporting actor tv movie':
+        lot_best_supporting_actor_tv = ['best', 'support', 'actor', 'series'] # Number 1 response with this
+        res = get_bigram_list_match_tweets_lax(tweets, lot_best_supporting_actor_tv)
+    
+    return get_best_match(res[0], parsed_list[category][:])
 
