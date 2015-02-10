93d7e1ef1cc4ee6a762212a942dd3c60c7a01349
TaoranYue <warriortryue@gmail.com>
2015-02-10 15:48:50 -0600

    small changes

--- project_01/src/GUI.py
+++ project_01/src/GUI.py
@@ -3,11 +3,11 @@ import json
 import os
 import main
 from main import *
-from read_tweets import *#read_in_tweets_2013, read_in_tweets_2015
-from scrape_2013_data import *#parse_2013_wikipedia_movies, parse_2013_wikipedia_tv, parse_2013_wikipedia_presenters
-from scrape_2015_data import *#parse_2015_wikipedia_movies, parse_2015_wikipedia_tv, parse_2015_wikipedia_presenters
-from match_options import *#get_bigram_list_match_tweets, get_bigram_list_match_tweets_lax, get_bigram_list_match_tweets_either_or_lax, get_unigram_list_match_tweets_lax, get_unigram_list_match_tweets_either_or_lax, pairThings
-from unused_files import *#get_academy_info # GET RID OF THIS BY USING SCRAPED DATA INSTEAD
+from read_tweets import *
+from scrape_2013_data import *
+from scrape_2015_data import *
+from match_options import *
+from unused_files import *
 root = Tk()
 root.title('Golden Globes Predictor')
 root.geometry('200x100')
@@ -15,26 +15,110 @@ root.geometry('200x100')
 def printOption(x):
 	if x == "Hosts":
 		print(v1.get())
-	if x == "Winners":
+	if x == "Best movie drama":
 		print(v1.get())
-	if x == "Awards":
+	if x == "Best actress drama":
 		print(v1.get())
-	if x == "Presenters":
+	if x == "Best actor drama":
 		print(v1.get())
-	if x == "Nominees":
+	if x == "Best movie musical or comedy":
 		print(v1.get())
-	if x == "Awards to Winners":
+	if x == "Best actress musical or comedy":
 		print(v1.get())
-	if x == "Awards to Presenters":
+	if x == "Best actor musical or comedy":
 		print(v1.get())
-	if x == "Awards to Nominees":
+	if x == "Best animated movie":
+		print(v1.get())
+	if x == "Best foreign movie":
+		print(v1.get())
+	if x == "Best supporting actress":
+		print(v1.get())
+	if x == "Best supporting actor":
+		print(v1.get())
+	if x == "Best director":
+		print(v1.get())
+	if x == "Best screenplay":
+		print(v1.get())
+	if x == "Best original score":
+		print(v1.get())
+	if x == "Best original song":
+		print(v1.get())
+	if x == "Best tv series drama":
+		print(v1.get())
+	if x == "Best actress tv drama":
+		print(v1.get())
+	if x == "Best actor tv drama":
+		print(v1.get())
+	if x == "Best tv musical or comedy":
+		print(v1.get())
+	if x == "Best actor tv musical or comedy":
+		print(v1.get())
+	if x == "Best tv movie":
+		print(v1.get())
+	if x == "Best actress tv movie":
+		print(v1.get())
+	if x == "Best actor tv movie":
+		print(v1.get())
+	if x == "Best supporting actress tv movie":
+		print(v1.get())
+	if x == "Best supporting actor tv movie":
 		print(v1.get())
 #Start function
 def run():
 	print'Please Wait for A Moment......'
 	x=v1.get()
 	if x == "Hosts":
-		print("This year's Hosts in Golden Globe Award are:")
+		print(v1.get())
+	if x == "Best movie drama":
+		print(v1.get())
+		winner = get_winner('best movie drama', tweets, parsed_list)
+        print(winner)
+	if x == "Best actress drama":
+		print(v1.get())
+	if x == "Best actor drama":
+		print(v1.get())
+	if x == "Best movie musical or comedy":
+		print(v1.get())
+	if x == "Best actress musical or comedy":
+		print(v1.get())
+	if x == "Best actor musical or comedy":
+		print(v1.get())
+	if x == "Best animated movie":
+		print(v1.get())
+	if x == "Best foreign movie":
+		print(v1.get())
+	if x == "Best supporting actress":
+		print(v1.get())
+	if x == "Best supporting actor":
+		print(v1.get())
+	if x == "Best director":
+		print(v1.get())
+	if x == "Best screenplay":
+		print(v1.get())
+	if x == "Best original score":
+		print(v1.get())
+	if x == "Best original song":
+		print(v1.get())
+	if x == "Best tv series drama":
+		print(v1.get())
+	if x == "Best actress tv drama":
+		print(v1.get())
+	if x == "Best actor tv drama":
+		print(v1.get())
+	if x == "Best tv musical or comedy":
+		print(v1.get())
+	if x == "Best actor tv musical or comedy":
+		print(v1.get())
+	if x == "Best tv movie":
+		print(v1.get())
+	if x == "Best actress tv movie":
+		print(v1.get())
+	if x == "Best actor tv movie":
+		print(v1.get())
+	if x == "Best supporting actress tv movie":
+		print(v1.get())
+	if x == "Best supporting actor tv movie":
+		print(v1.get())
 	if x == "Winners":
 		print("This year's Golden Globe Winners are")
 	if x == "Awards":
@@ -49,28 +133,61 @@ def run():
 		print("This year's Golden Globe Awards to Presenters are:")
 	if x == "Awards to Nominees":
 		print("This year's Golden Globe Awards to Nominees are:")
+	if x == "Awards":
+		print(v1.get())
+	if x == "Presenters":
+		print(v1.get())
+	if x == "Nominees":
+		print(v1.get())
+	if x == "Awards to Winners":
+		print(v1.get())
+	if x == "Awards to Presenters'":
+		print(v1.get())
+	if x == "Awards to Nominees'":
+		print(v1.get())
 	os.system('python main.py')
-#Number function
-def func(event):
-    print("You hit return.")
 #Write file function
 os.system('python save_output.py')
 
 v1 = StringVar(root)
 v1.set('GoldenGlobe')
-om1 = OptionMenu(root,v1,'Hosts','Winners','Awards','Presenters','Nominees','Awards to Winners','Awards to Presenters','Awards to Nominees',command=printOption)
+om1 = OptionMenu(root,v1
+,'Hosts'
+,'Best movie drama'
+,'Best actress drama'
+,'Best actor drama'
+,'Best movie musical or comedy'
+,'Best actress musical or comedy'
+,'Best actor musical or comedy'
+,'Best animated movie'
+,'Best foreign movie'
+,'Best supporting actress'
+,'Best supporting actor'
+,'Best director'
+,'Best screenplay'
+,'Best original score'
+,'Best original song'
+,'Best tv series drama'
+,'Best actress tv drama'
+,'Best actor tv drama'
+,'Best tv musical or comedy'
+,'Best actor tv musical or comedy'
+,'Best tv movie'
+,'Best actress tv movie'
+,'Best actor tv movie'
+,'Best supporting actress tv movie'
+,'Best supporting actor tv movie'
+,'Winners'
+,'Awards'
+,'Presenters'
+,'Nominees'
+,'Awards to Winners'
+,'Awards to Presenters'
+,'Awards to Nominees',
+command=printOption)
 om1.bind("Button-1>",printOption)
 om1.grid(row=0,column=0,columnspan = 2,stick = E + W+ N + S)
 e = StringVar()
-#entry = Entry(root,textvariable = e)
-#e.set('1')
-#entry.bind("<Return>",func)
-#entry.grid(row=1,column=1)
-#Label(root,text="Number:").grid(row=1,column=0,sticky=W)
-#Label(root,text="Result:").grid(row=3,sticky=W)
-#root.lbResults = Listbox(root)
-#root.lbResults.bind(("Button-1>",results))
-#root.lbResults.grid(row = 4,column = 0,columnspan = 2,stick = E + W + N + S)
 json = Button(root,text='WRITE FILE',activeforeground='white',activebackground='red')
 json.grid(row=6,column=1,stick =E + W+ N + S)
 quit = Button(root,text='QUIT',command=root.quit,activeforeground='white',activebackground='red')
