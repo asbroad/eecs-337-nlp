57580bb92cc9dd12c8b8e98593ad7f985c5b04a5
Omnei <sethm1093@gmail.com>
2015-02-15 19:52:29 -0600

    Updated Fun Stuff
    Now runs standalone.  Added histogram interp

--- project_01/src/fun_thing.py
+++ project_01/src/fun_thing.py
@@ -14,33 +14,36 @@ import random
 ignore_list = ['will', 'ferrell', 'kristen', 'wiig', 'bill', 'hader', 'golden', 'globes', 'globe', 'goldenglobes', '#goldenglobes', 'oscars']
 
 
-def find_subset(name, tweets):
+def find_subset(name, tweetsIn):
 	subSet = []
-	for tweet in tweets:
+	for tweet in tweetsIn:
 		tweetText = tweet[0].lower()
 		if match_all_words(tweetText, name.lower().split()):
 			subSet.append(tweet)
-	print("subSet Generated")
 	return subSet
 
 
-def gg_typical_tweet(name, tweets):
-	subSet = find_subset(name, tweets)
-	random.shuffle(subSet)
-	subSet = subSet[0:1000]
-	print("sampled")
-	top_hashtags = find_hashtag(subSet)
-	other = get_bigram_list_match_tweets(subSet, name.split())[1][0]
-	hashtags = random.sample(set(top_hashtags[0:10]), 3)
-
+def gg_typical_tweet(name, tweetsIn):
+	if(len(tweetsIn) < 100):
+		print("Warning: Low Number of tweets. Tweet will be partially generated with default values.")
+	random.shuffle(tweetsIn)
+	sampledTweets = tweetsIn[0:min(2000, len(tweetsIn))]
+	top_hashtags = find_hashtag(sampledTweets)
+	other = get_bigram_list_match_tweets(sampledTweets, name.split())[1][0]
+	if len(top_hashtags) >= 3:
+		hashtags = random.sample(set(top_hashtags[0:min(10, len(top_hashtags))]), 3)
+	else:
+		hashtags = [("#goldenglobes", -1), ("#redcarpet", -1), ("#gg", -1)]
+		for idx in range(0,len(top_hashtags)):
+			hashtags[idx] = top_hashtags[idx]
 	myTweet = "{} at the #goldenglobes2015 is amazing.  Looking forward to {}! {} {} {}".format(name, other, hashtags[0][0], hashtags[1][0], hashtags[2][0])
-	print(myTweet)
+	return myTweet
 
 
 
-def find_hashtag(tweets):
+def find_hashtag(tweetsIn):
 	d = defaultdict(int)
-	for tweet in tweets:
+	for tweet in tweetsIn:
 		tweetText = tweet[0].lower().split()
 		for token in tweetText:
 			#print(token)
@@ -48,6 +51,7 @@ def find_hashtag(tweets):
 				d[token] += 1
 
 	for key in ignore_list:
+
 		for dKey in d.keys():
 			if key in dKey:
 				del d[dKey]
@@ -58,11 +62,11 @@ def find_hashtag(tweets):
 
 
 
-def gg_hist(name, tweets, max_bins = 20, start_time = datetime(2015, 1, 11, 16, 0 ,0)):
-	subSet = find_subset(name, tweets)
+def gg_hist(name, tweetsIn, max_bins = 20, start_time = datetime(2015, 1, 11, 16, 0 ,0)):
+	#subSet = find_subset(name, tweets)
 	bins = [list() for _ in xrange(max_bins)]
 
-	for tweet in subSet:
+	for tweet in tweetsIn:
 		bin = classifyTimeStamp(tweet, max_bins, start_time)
 		bins[bin].append(tweet)
 
@@ -93,21 +97,47 @@ def printHist(histogram, start_time = datetime(2015, 1, 11, 16, 0 ,0), bin_size 
 	for idx in range(0, len(histogram)):
 		bin_time = start_time + idx * bin_size
 		pretty = "{} - {}".format(bin_time, len(histogram[idx]))
+		if idx == 0:
+			print("-Red Carpet-")
+		elif idx == 8:
+			print("-Main Event-")
+		elif idx == 17:
+			print("-Afterparties-")
+
 		print(pretty)
 
-'''
+def interpHist(name, hist):
+	maxBin = 0
+	maxBinSize = 0
+	for idx in range(0, len(hist)):
+		if len(hist[idx]) > maxBinSize:
+			maxBinSize = len(hist[idx])
+			maxBin = idx
+	
+	if maxBin < 6:
+		print("{} was a huge hit on the Red Carpet".format(name))
+	elif maxBin < 17:
+		print("{} stole the show at the Award Ceremony".format(name))
+	else:
+		print("{} was the life of the Afterparty".format(name))
+
+
 def main():
 	year = '2015'
+	name = raw_input("Enter name of Actor/Actress: ")
+	print("Loading Tweets for {}".format(name))
 	[tweets, parsed_list, parsed_presenter_list] = load_data(year)
-
-
-	#res = find_hashtag(tweets)
-	#print(res[0:5])
-	#gg_typical_tweet("Tina Fey", tweets)
-	#bin = classifyTimeStamp(tweets[0])
-	#print(bin)
-	hist = gg_hist("Tina Fey", tweets)
+	subSet = find_subset(name, tweets)
+	print("Histogram of Twitter Activity for {}: ".format(name))
+	hist = gg_hist(name, subSet)
 	printHist(hist)
+	interpHist(name, hist)
+	print("Generating Tweet for {}:".format(name))
+	customTweet = gg_typical_tweet(name, subSet)
+	print(customTweet)
+	
+
+
 
 def load_data(year='2013'):
     if year == '2013':
@@ -138,4 +168,3 @@ def load_data(year='2013'):
 if __name__ == "__main__":
     main()
 
-'''
