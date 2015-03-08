0983ea5a7c9dc6fece4df8214b258afcc40e3a04
Omnei <sethm1093@gmail.com>
2015-03-08 12:51:16 -0500

    added recipe class

---
+++ project_02/src/recipe.py
@@ -0,0 +1,7 @@
+class Recipe:
+	def __init__(self, name, ingredients, tools, methods, directions):
+		self.name = name
+		self.ingredients = ingredients
+		self.tools = tools
+		self.methods = methods
+		self.directions = directions
