481e9e4b17ba79a98c389a493d086b5a2fc7fa38
Omnei <sethm1093@gmail.com>
2015-03-08 15:01:07 -0500

    Added Recipe parse to recipe class

--- project_02/src/recipe.py
+++ project_02/src/recipe.py
@@ -1,7 +1,8 @@
 class Recipe:
-	def __init__(self, name, ingredients, tools, methods, directions):
+	def __init__(self, name, ingredients, tools, main_method, other_methods, directions):
 		self.name = name
 		self.ingredients = ingredients
 		self.tools = tools
-		self.methods = methods
+		self.main_method = main_method
+		self.other_methods = other_methods
 		self.directions = directions
